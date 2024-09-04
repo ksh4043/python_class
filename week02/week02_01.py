7ce3c072733468f9dab897358039b3844d2ad3ef
ksh <119904568+ksh4043@users.noreply.github.com>
2024-09-04 16:01:30 +0900

    Add files via upload

---
+++ week02/week02_01.py
@@ -0,0 +1,9 @@
+# week02_01.py
+
+a = 3 * 10
+print(a)
+
+a = "안녕" * 3
+print(a)
+
+if 4 in [1,2,3,4] : print("4있다")
---
+++ week02/week02_02.py
@@ -0,0 +1,26 @@
+# week02_02.py
+
+
+print("내년에는 몇 살일까?")
+age = int(input("나이 입력 : "))
+print(age+1)
+
+print("#4")
+score1 = input("점수1 : ")
+score2 = input("점수2 : ")
+print(score1 + score2)
+
+print("#5")
+score1 = int(input("점수1 : "))
+score2 = int(input("점수2 : "))
+print(score1 + score2)
+
+print("#1")
+input()
+
+print("#2")
+input("점수 : ")
+
+print("#3")
+a = input("점수 : ")
+print(a)
